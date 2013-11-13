b4daf2e5a539deb0c87671da69cea8c9ca03a97e
KelliOrrela <kelli.orrela@gmail.com>
2013-11-13 10:52:40 +0200

    initial commit

---
+++ application.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    
+	$('#basecamp').hover(function() {
+        $('#basecampslogan').toggle();
+        $('#sloganmain').toggle();
+        $('#slogansub').toggle();
+    });
+
+    $('#highrise').hover(function() {
+        $('#highriseslogan').toggle();
+        $('#sloganmain').toggle();
+        $('#slogansub').toggle();
+     });
+
+    $('#campfire').hover(function() {
+       	$('#campfireslogan').toggle();
+       	$('#sloganmain').toggle();
+       	$('#slogansub').toggle();
+     });
+
+});
