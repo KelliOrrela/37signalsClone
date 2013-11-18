2b49ecd163041d35660a120abf78707d145bf175
KelliOrrela <kelli.orrela@gmail.com>
2013-11-18 17:22:21 +0200

    div selector modified
    to be more maintainable re. Pete's suggestion

--- application.js
+++ application.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
+
+  $('div').hover(function() {
+    var sloganToToggleID = '#' + $(this).attr('id') + 'slogan';
+    $(sloganToToggleID).toggle()
     
-	$('#basecamp').hover(function() {
-        $('#basecampslogan').toggle();
-        $('#sloganmain').toggle();
-        $('#slogansub').toggle();
+    $('#sloganmain').toggle();
+    
+    $('#slogansub').toggle();
     });
-
-    $('#highrise').hover(function() {
-        $('#highriseslogan').toggle();
-        $('#sloganmain').toggle();
-        $('#slogansub').toggle();
-     });
-
-    $('#campfire').hover(function() {
-       	$('#campfireslogan').toggle();
-       	$('#sloganmain').toggle();
-       	$('#slogansub').toggle();
-     });
 
 });
